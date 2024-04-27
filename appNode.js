75ee849dcfb5f98cca7b16bc41ad92b4a58b821e
DavidAlvrz <davidalvarezm95@gmail.com>
2024-04-27 23:13:18 +0200

    Main page

--- appNode.js
+++ appNode.js
@@ -2,33 +2,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
-app.get('/add/:p1/:p2', (req, res) => {
-    const p1 = parseInt(req.params.p1);
-    const p2 = parseInt(req.params.p2);
-    const total = p1 + p2;
-    res.send('Result is : ' + total);
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/sub/:p1/:p2', (req, res) => {
-    const p1 = parseInt(req.params.p1);
-    const p2 = parseInt(req.params.p2);
-    const total = p1 - p2;
-    res.send('Result is : ' + total);
-});
-
-app.get('/mult/:p1/:p2', (req, res) => {
-    const p1 = parseInt(req.params.p1);
-    const p2 = parseInt(req.params.p2);
-    const total = p1 * p2;
-    res.send('Result is : ' + total);
-});
-
-app.get('/div/:p1/:p2', (req, res) => {
-    const p1 = parseInt(req.params.p1);
-    const p2 = parseInt(req.params.p2);
-    const total = p1 / p2;
-    res.send('Result is : ' + total);
+app.get('/app', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/app.html'));
 });
 
 app.listen(port, () => {
